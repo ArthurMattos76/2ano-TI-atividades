eeb1c526f8728bb99abd5b8213c996189445688b
Arthur Mattos <tucaarrow@gmail.com>
2026-06-02 14:23:13 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,16 +2,37 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
+@app.route('/cadastro', methods=['GET', 'POST'])
+def cadastro():
 
-@app.route('/login', methods=['POST'])
-def recebedados():
-    nome = request.form['nome']
-    email = request.form['email']   
+    mensagem = ''
 
-    return "{} e {}".format(nome, email)
+    if request.method == 'POST':
+        nome = request.form['nome']
+        if not nome:
+            mensagem = "O campo nome é obrigatório!"
+        else:
+            mensagem = f"Cadastro realizado com sucesso! Bem-vindo, {nome}"
+    
+    return render_template('cadastro.html', mensagem=mensagem)
+app.debug = True
 
-if __name__ == '__main__':
+@app.route("/")
+def formulario():
+    return render_template("index.html")
+
+@app.route("/validacao", methods=["POST"])
+def validacao():
+
+    nome = request.form.get('nome','').strip().title()
+    email = request.form.get('email','').strip().lower()
+    senha = request.form.get('senha','').strip().title()
+
+    return f"""  
+    Nome:{nome}<br>
+    Email:{email}<br>
+    Senha:{senha}
+    """
+
+if __name__ == "__main__":
     app.run(debug=True)
